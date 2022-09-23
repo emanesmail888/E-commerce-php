0a6aecd634cb2566fb64ee3d3966a2350d108d1a
emanesmail888 <103720290+emanesmail888@users.noreply.github.com>
2022-09-24 01:54:06 +0200

    Add files via upload

---
+++ admin_area/include/footer.php
@@ -0,0 +1,95 @@
+<div id="footer">
+    <div class="container">
+        <div class="row">
+        <div class="col-md-3 col-sm-6">
+            <h4>pages</h4>
+<ul>
+    <li><a href="cart.php">Shopping Cart</a></li>
+    <li><a href="contact.php">Contact us</a></li>
+    <li><a href="shop.php">Shop </a></li>
+    <li><a href="checkout.php">My Account  </a></li>
+    
+</ul>  
+<h4>User Section</h4> 
+<ul>
+
+    <li><a href="customer_register.php">Register </a></li>
+    <li><a href="checkout.php">Login  </a></li>
+
+</ul>        
+<hr class="hidden-md hidden-lg hidden-sm"> 
+
+
+
+
+</div> <!-- col-md-3 -->   
+
+
+<div class="col-md-3 col-sm-6">
+            <h4>categories</h4>
+<ul>
+    <li><a href="#">Shopping Cart</a></li>
+    <li><a href="#">Contact us</a></li>
+    <li><a href="#">Shop </a></li>
+    <li><a href="#">My Account  </a></li>
+    
+</ul>  
+    
+<hr class="hidden-md hidden-lg hidden-sm"> 
+
+
+
+
+</div> <!-- col-md-3 -->   
+
+
+
+<div class="col-md-3 col-sm-6">
+    <h4> Where To Find Us</h4>
+    <p>
+        <strong>Computer Fast</strong>
+        <br>cairo
+        <br>Egypt
+        <br>010034562177
+        <br>negma.oula@yahoo.com_create_guid
+        <strong>Eman Esmail</strong>
+    </p>
+    <a href="contact.php">Go To Contact Us Page</a>
+    <hr class="hidden-md hidden-lg hidden-sm"> 
+
+
+
+</div><!-- col-md-3 -->
+
+
+
+
+<div class="col-md-3 col-sm-6">
+    <h4>Get The News</h4>
+    <p class="text-muted">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, alias.</p>
+    <form action="" class="mb-4" >
+        <div class="input-group">
+            
+            <input class="form-control" type="text" name="email" >
+            <div class="input-group-append">
+                <span class="input-group-btn">
+                    <input type="submit" class="btn btn-secondary" value="subscribe">
+                </span>
+            </div>
+        </div>
+
+    </form>
+
+                 <ul class=" social ">
+                                    <li><a href="#"><i class="fab fa-facebook-f "></i></a></li>
+                                    <li><a href="#"><i class="fab fa-twitter "></i></a></li>
+                                    <li><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-dribbble"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-google-plus-g"></i></a></li>
+             </ul>
+
+</div><!-- col-md -3 -->
+
+        </div><!-- row -->
+    </div><!-- container -->
+</div><!-- footer -->
